e544b4222b62a754f26300a85f95182d50c353ca
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-06-15 21:49:46 +0200

    add _blank

--- src/components/social.svelte
+++ src/components/social.svelte
@@ -8,6 +8,8 @@
 	href={link}
 	class={`flex justify-center hover:text-accent duration-100 ${tooltip ? 'tooltip' : ''}`}
 	data-tip={tooltip}
+	target="_blank"
+	rel="nofollow"
 >
 	<svelte:component this={icon} width="3em" />
 </a>
