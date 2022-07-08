ad07f42a95797ff6cc3081369a91c359c98f0cfd
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-07-08 23:38:40 +0200

    stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,8 +15,6 @@
 	import { tech } from './tech.json';
 	import '../app.css';
 
-	let display = false;
-	let mode: string | number = 2;
 	let text = 'Welcome to Tricked.pro this is a awesome site';
 	let slide = 5;
 	let title = text.substring(0, slide);
@@ -44,13 +42,6 @@
 		<div>
 			<h1 class="text-5xl font-bold">Tricked.pro</h1>
 			<p class="py-6">Welcome to my place in the internet</p>
-			<button
-				class="btn"
-				on:click={() => {
-					display = !display;
-					setTimeout(() => document.getElementById('hire').scrollIntoView(), 50);
-				}}>Sponsor Tiers</button
-			>
 		</div>
 		<div>
 			<div class="card w-96 bg-neutral text-neutral-content py-4">
@@ -96,54 +87,3 @@
 		</div>
 	</div>
 </div>
-{#if display}
-	<div class="bg-base-300 m-h-screen py-5" id="hire">
-		<div class="flex-col lg:flex-row">
-			<div class="flex justify-center">
-				<div class="card">
-					<div class="card-body w-96">
-						<h2 class="text-3xl font-bold">hire me</h2>
-						<p class="py-6">
-							If you want to hire me please contact me on discord or email. Otherwise if you just
-							want to support my work you can view my github sponsor tiers below and on my gihtub
-							profile!
-						</p>
-					</div>
-				</div>
-			</div>
-
-			<div class="justify-center flex flex-col ">
-				<h2 class="prose max-w-none text-center">Github Sponsor Tiers</h2>
-				<div class="tabs justify-center">
-					{#each [[1, 'All'], [2, 'OneTime'], [3, 'Monthly']] as i}
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<a class={`tab ${mode == i[0] ? 'tab-active' : ''}`} on:click={() => (mode = i[0])}
-							>{i[1]}</a
-						>
-					{/each}
-				</div>
-				<div class="grid justify-center gap-4 auto-rows-min">
-					{#each tiers.data.user.sponsorsListing.tiers.edges.filter( (x) => (mode == 1 ? true : mode == 2 ? x.node.isOneTime : !x.node.isOneTime) ) as tier}
-						<div class="card w-96 bg-base-100 shadow-xl">
-							<div class="card-body">
-								<h2 class="card-title">{tier.node.name}</h2>
-								<p class="text-start prose">
-									{@html tier.node.descriptionHTML}
-								</p>
-								<div class="card-actions justify-end mt-auto">
-									<a
-										href={`https://github.com/sponsors/Tricked-dev${
-											tier.node.isOneTime ? '?frequency=one-time' : ''
-										}`}
-									>
-										<button class="btn btn-primary">View on Github!</button>
-									</a>
-								</div>
-							</div>
-						</div>
-					{/each}
-				</div>
-			</div>
-		</div>
-	</div>
-{/if}
