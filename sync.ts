099fa236c8187816d378e69a2e6b604ebeee0510
Tricked Dev <tricked@tricked.dev>
2025-06-21 01:03:35 +0200

    yes

--- sync.ts
+++ sync.ts
@@ -1,5 +1,4 @@
 import { S3Client } from "bun"
-import { readdir } from "fs/promises"
 
 const s3 = new S3Client({
     endpoint: process.env.S3_ENDPOINT!,
@@ -9,34 +8,24 @@ const s3 = new S3Client({
     accessKeyId: process.env.S3_KEY!,
 })
 
-
 let allNames: string[] = [];
 
-async function laodData(key?: string) {
+async function loadData(key?: string) {
     let data = await s3.list(key ? {
         continuationToken: key
     } : undefined);
     allNames.push(...data.contents?.map(x => x.key) ?? []);
     for (const x of data.contents ?? []) {
-        if (x.key.endsWith(".png.png")) {
+        if (x.key.endsWith(".png.png") || x.key.startsWith("to_process/")) {
             await s3.delete(x.key)
         }
     }
     if (data.isTruncated) {
-        await laodData(data.nextContinuationToken)
+        await loadData(data.nextContinuationToken)
     }
 }
 
-await laodData()
+await loadData()
 
 
-await Bun.write("public/allNames.txt", allNames.join("\n"))
-
-
-let promises = []
-for (const file of await readdir(`${process.env.HOME}/Backgrounds`)) {
-    if (!file.endsWith(".png") || allNames.includes(file)) continue;
-    promises.push(s3.write(file, new Blob([await Bun.file(`${process.env.HOME}/Backgrounds/${file}`).arrayBuffer()], { type: "image/png" })))
-}
-
-await Promise.all(promises)
+await Bun.write("public/allNames.json", JSON.stringify(allNames))
